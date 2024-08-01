d04a2a4eae8cb18529aa928d9f526427ec386703
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-08-01 18:19:14 +0200

    feat: add projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,16 +1,19 @@
-function ProjectCard(props){
+function ProjectCard({projects}){
+    
     return(
         <>
         <div className="max-w-sm border-2 border-gray-200 rounded-lg shadow-md">
-            <img src="src/assets/ReactPortfolioWebsite.png" 
+            <img src={`${projects.image}`} 
             className="rounded-t-lg"
             width="400px"></img>
             <div className="p-4">
-                <h2 className="text-3xl mb-2">Project Name</h2>
+                <h2 className="text-3xl mb-2">{projects.title}</h2>
                 <p className="mb-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                
+                {projects.description}
                 </p>
-                <a className="bg-orange-500 text-white px-4 py-3 inline-flex justify-center items-center text-center rounded-md shrink-0 ">
+                <a href={`${projects.link}`}
+                className="bg-orange-500 text-white px-4 py-3 inline-flex justify-center items-center text-center rounded-md shrink-0 ">
                     Go to Github
                 </a>
             </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,9 @@
-import ProjectCard from "../components/ProjectCard"
+import ProjectCard from "../components/ProjectCard";
+import projects from "../projects.json";
 
 function Projects(){
+
+    console.log(projects);
     return(
         <>
         <div className="max-w-screen-xl mx-auto my-5">
@@ -9,7 +12,14 @@ function Projects(){
                 <h2 className="text-6xl font-semibold my-14">Projects</h2>
             </div>
             <div className="flex flex-row flex-wrap justify-center  md:grid-cols-3 md:gap-9 gap-y-10">
-            <ProjectCard />
+                {
+                    projects.map((project) => (
+                        <ProjectCard key={project.id} projects={project} />
+                    ))
+                        
+                    
+                }
+            {/* <ProjectCard /> */}
             
             </div>
         </div>

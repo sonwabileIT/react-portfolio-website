912038f925c408d312917800313183e2e7a0b120
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-08-01 14:23:42 +0200

    feat: add comming soon message

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,12 @@
 function Projects(){
     return(
         <>
-            <h2>Projects</h2>
-            <h2>Comming Soon...</h2>
+        <div className="max-w-screen-xl mx-auto p-20 md:p-44">
+            <div className="text-center">
+                <h2 className="text-6xl font-semibold">Projects</h2>
+                <h2 className="text-4xl mt-2 font-medium">Comming Soon...</h2>
+            </div>
+        </div>
         </>
     )
 }

373b022b14f0c9750cb7a5635414a5adca95aaaf
Sonwabile <sonwabilegxoyiyait@gmail.com>
2025-06-21 21:44:49 +0200

    ui: project cards more responsive

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,29 +1,29 @@
 import ProjectCard from "../components/ProjectCard";
 import projects from "../projects.json";
 
-function Projects(){
+function Projects() {
 
-    return(
-        <>
-        <div className="max-w-screen-xl mx-auto my-5">
-            
-            <div className="text-center">
-                <h2 className="text-6xl font-semibold my-14">Projects</h2>
-            </div>
-            <div className="flex flex-row flex-wrap justify-center  md:grid-cols-3 md:gap-9 gap-y-10">
-                {
-                    projects.map((project) => (
-                        <ProjectCard key={project.id} projects={project} />
-                    ))
-                        
-                    
-                }
-            
-            </div>
-        </div>
-        
-        </>
-    )
+	return (
+		<>
+			<div className="max-w-screen-xl mx-auto my-5">
+
+				<div className="text-center">
+					<h2 className="text-6xl font-semibold my-16">Projects</h2>
+				</div>
+				<div className="flex flex-row flex-wrap justify-center md:justify-center md:gap-9 gap-y-10 lg:justify-center lg:mx-auto">
+					{
+						projects.map((project) => (
+							<ProjectCard key={project.id} projects={project} />
+						))
+
+
+					}
+
+				</div>
+			</div>
+
+		</>
+	)
 }
 
 export default Projects
